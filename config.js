8e3ac2ac744dd107484f13a2eb47117004ca5c7d
tammawrk-png <tammawrk@gmail.com>
2026-08-14 11:07:08 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Firebase Web config ของบัญชี tammawrk@gmail.com
+window.APP_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyBWyU80GjubCrVJ8e8dtdmZN7V6WY5E-uU",
+    authDomain: "certificate-number-search-th.firebaseapp.com",
+    databaseURL: "https://certificate-number-search-th-default-rtdb.firebaseio.com",
+    projectId: "certificate-number-search-th",
+    storageBucket: "certificate-number-search-th.firebasestorage.app",
+    messagingSenderId: "337114497187",
+    appId: "1:337114497187:web:b2ef748ad0d9794c0ebf16"
+  },
+  // ไม่เผยแพร่รหัสโฟลเดอร์ Drive ใน Public repository
+  googleDriveFolderId: null
+};
